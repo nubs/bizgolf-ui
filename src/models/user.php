47ad0e5d745e76bd7b9de93c7054d32954cd8fe7
Spencer Rinehart <spencer.rinehart@dominionenterprises.com>
2013-07-04 13:09:20 -0400

    Add registration.

--- src/models/user.php
+++ src/models/user.php
@@ -6,5 +6,12 @@ return function(MongoDB $db) {
         'exists' => function(array $conditions = array()) use($collection) {
             return $collection->findOne($conditions) !== null;
         },
+        'create' => function(array $fields) use($collection) {
+            try {
+                $collection->insert($fields);
+            } catch (Exception $e) {
+                throw new Exception('Failed to create user.');
+            }
+        },
     ];
 };
--- src/routes.php
+++ src/routes.php
@@ -3,6 +3,9 @@ return function(\Slim\Slim $app, array $models, array $middleware) {
     $loginRoute = require 'routes/login.php';
     $loginRoute($app, $models['user']);
 
+    $registerRoute = require 'routes/register.php';
+    $registerRoute($app, $models['user']);
+
     $homeRoute = require 'routes/home.php';
     $homeRoute($app, $models['hole'], $middleware['loadAuth']);
 
