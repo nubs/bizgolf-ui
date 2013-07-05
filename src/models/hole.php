a9a0855aad3589a8736d345c2c2c95f180c7cd8b
Spencer Rinehart <spencer.rinehart@dominionenterprises.com>
2013-07-05 17:30:08 -0400

    Add scoreboard for a hole.
    A lot of logic is in this one.  The partitionByUser bit is there because
    we are going to want to use it in the future for when a user is trying
    to view all of their submissions for a hole easily (there are other
    ways, but it would allow some sharing of code).  Only a user's best
    score for the hole is counted and the scores are generated with the best
    score getting a 1000 score, in the same way as most codegolf websites.

--- src/models/hole.php
+++ src/models/hole.php
@@ -2,7 +2,67 @@
 return function(MongoDB $db) {
     $collection = $db->holes;
 
-    $findOne = function($id) use($collection) {
+    $partitionByUser = function(array $submissions) {
+        $users = [];
+        foreach ($submissions as $submission) {
+            $username = $submission['username'];
+            if (array_key_exists($username, $users)) {
+                $users[$username][] = $submission;
+            } else {
+                $users[$username] = [$submission];
+            }
+        }
+
+        return $users;
+    };
+
+    $bestForEachUser = function(array $submissions) use($partitionByUser) {
+        $users = $partitionByUser($submissions);
+        $result = [];
+
+        foreach ($users as $username => $userSubmissions) {
+            $passingSubmissions = array_filter($userSubmissions, function($submission) {
+                return $submission['result'];
+            });
+
+            if (!empty($passingSubmissions)) {
+                $result[$username] = array_reduce($passingSubmissions, functioN($min, $submission) {
+                    if ($min === null || $submission['length'] < $min['length']) {
+                        return $submission;
+                    }
+
+                    return $min;
+                });
+
+            }
+        }
+
+        usort($result, function($a, $b) {
+            if ($a['length'] === $b['length']) {
+                return 0;
+            }
+
+            return $a['length'] < $b['length'] ? -1 : 1;
+        });
+
+        return $result;
+    };
+
+    $addScores = function(array $sortedSubmissions) {
+        if (empty($sortedSubmissions)) {
+            return $sortedSubmissions;
+        }
+
+        $bestLength = (float)$sortedSubmissions[0]['length'];
+
+        foreach ($sortedSubmissions as &$submission) {
+            $submission['score'] = (int)($bestLength * 1000.0 / (float)$submission['length']);
+        }
+
+        return $sortedSubmissions;
+    };
+
+    $findOne = function($id) use($collection, $bestForEachUser, $addScores) {
         $hole = null;
         try {
             $hole = $collection->findOne(['_id' => new MongoID($id)]);
@@ -12,6 +72,8 @@ return function(MongoDB $db) {
         if ($hole === null) {
             throw new Exception("Hole '{$id}' does not exist.");
         }
+
+        $hole['scoreboard'] = $addScores($bestForEachUser($hole['submissions']));
 
         return $hole;
     };
