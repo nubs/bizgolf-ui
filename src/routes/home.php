3db5d0a699ed6c0daa6389ac2b4adb4c9fb01f32
Spencer Rinehart <spencer.rinehart@dominionenterprises.com>
2013-07-08 12:57:21 -0400

    Add recent scores to homepage.

--- src/routes/home.php
+++ src/routes/home.php
@@ -1,6 +1,24 @@
 <?php
 return function(\Slim\Slim $app, array $holeModel, array $userModel, callable $loadAuth) {
     $app->get('/', $loadAuth, function() use($app, $holeModel, $userModel) {
-        $app->render('home.html', ['holes' => $holeModel['find'](), 'users' => $userModel['find'](), 'user' => $app->config('codegolf.user')]);
+        $holes = $holeModel['find']();
+        $submissions = [];
+        foreach ($holes as $hole) {
+            foreach ($hole['submissions'] as $submission) {
+                $submission['hole'] = $hole;
+                $submissions[] = $submission;
+            }
+        }
+
+        usort($submissions, function($a, $b) {
+            return $b['_id']->getTimestamp() - $a['_id']->getTimestamp();
+        });
+
+        $submissions = array_slice($submissions, 0, 20);
+
+        $app->render(
+            'home.html',
+            ['holes' => $holes, 'users' => $userModel['find'](), 'user' => $app->config('codegolf.user'), 'submissions' => $submissions]
+        );
     })->name('home');
 };
